85c89a0fbbb2a02bc3b3c29b3b67e26b6fd65824
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-14 12:49:14 -0400

    OperatorsExercise

---
+++ OperatorsExercise/OperatorsExerciseMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExercise
+{
+    internal class OperatorsExerciseMethod
+    {
+
+     public static int Modulus (int num1, int num2) 
+
+
+            { return num1 % num2; }
+
+        // 7 / 5 =1r2 
+        public static int Add (int num1, int num2)
+
+
+        { return num1 + num2; }
+
+        // 7 + 5 =12
+
+        public static int subtract (int num1, int num2)
+
+
+        { return num1 - num2; }
+
+        // 7 - 5 = 2
+
+        public static int Division (int num1, int num2)
+
+
+        { return num1 / num2; }
+
+        // 8 / 4 = 2 
+
+    }
+}
---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using OperatorsExercise;
+using System;
+
+Console.WriteLine(OperatorsExerciseMethod.Modulus(7, 5));
+
+Console.WriteLine(OperatorsExerciseMethod.Add (7, 5));
+
+Console.WriteLine(OperatorsExerciseMethod.subtract (7, 5));
+
+Console.WriteLine(OperatorsExerciseMethod.Division (8, 4));
